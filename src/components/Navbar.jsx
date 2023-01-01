09060bc31b816d39aa7a84605445c68c00ca1c23
wch <1165553576@qq.com>
2023-01-01 23:33:14 +0800

    完成context上下文提供器

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { signOut } from "firebase/auth"
+import { auth } from '../firebase'
 
 const Navbar = () => {
     return (
@@ -7,7 +9,7 @@ const Navbar = () => {
             <div className="user">
                 <img src="https://uploadfile.bizhizu.cn/2017/0701/20170701031430776.jpg" alt="" />
                 <span>john</span>
-                <button >退出登录</button>
+                <button onClick={() => signOut(auth)}>退出登录</button>
             </div>
         </div>
     )
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,10 +4,12 @@ import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth, db, storage } from "../firebase";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore";
+import { useNavigate } from 'react-router-dom';
 
 
 const Register = () => {
-    const [err, setErr] = useState(false)
+    const [err, setErr] = useState(false);
+    const navigate = useNavigate()
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -43,9 +45,11 @@ const Register = () => {
                             email,
                             photoURL: downloadURL,
                         });
+
+                        await setDoc(doc(db, "userChats", res.user.uid), {});
+                        navigate("/");
                     });
-                }
-            );
+                });
         } catch (err) {
             setErr(true);
         }
