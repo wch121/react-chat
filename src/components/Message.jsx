cba87656494cb6a53f3b5f1067ff68ef3106cdc5
wch <1165553576@qq.com>
2023-01-04 23:59:06 +0800

    完成聊天信息输入和展示

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useContext, useEffect, useRef } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
 
-const Message = () => {
+const Message = ({ message }) => {
+    const { currentUser } = useContext(AuthContext);
+    const { data } = useContext(ChatContext);
+
+    const ref = useRef();
+
+    useEffect(() => {
+        ref.current?.scrollIntoView({ behavior: "smooth" });
+    }, [message]);
+
     return (
-        <div className="message owner">
+        <div ref={ref} className={`message ${message.senderId === currentUser.uid && "owner"}`}>
             <div className="messageInfo">
                 <img
-                    src="https://uploadfile.bizhizu.cn/2017/0701/20170701031430776.jpg"
+                    src={
+                        message.senderId === currentUser.uid
+                            ? currentUser.photoURL
+                            : data.user.photoURL
+                    }
                     alt=""
                 />
                 <span>刚刚</span>
             </div>
             <div className="messageContent">
-                <p>hello</p>
-                <img src="https://uploadfile.bizhizu.cn/2017/0701/20170701031430776.jpg" alt="" />
+                <p>{message.text}</p>
+                {message.img && <img src={message.img} alt="" />}
             </div>
         </div>
     )
