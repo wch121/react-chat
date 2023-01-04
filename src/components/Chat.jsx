a04c83d4483cb788559805ef92ce7f618ef75971
wch <1165553576@qq.com>
2023-01-04 22:25:31 +0800

    完成侧边栏用户信息展示和选择

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
+import React,{ useContext } from 'react'
 import Messages from './Messages'
 import Input from './Input'
 import Cam from "../img/cam.png";
 import Add from "../img/add.png";
 import More from "../img/more.png";
+import { ChatContext } from "../context/ChatContext";
 
-const chat = () => {
+const Chat = () => {
+    const { data } = useContext(ChatContext);
+
     return (
         <div className='chat'>
             <div className="chatInfo">
-                <span>Jane</span>
+                <span>{data.user?.displayName}</span>
                 <div className="chatIcons">
                     <img src={Cam} alt="" />
                     <img src={Add} alt="" />
@@ -22,4 +25,4 @@ const chat = () => {
     )
 }
 
-export default chat
+export default Chat
--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,31 +1,48 @@
-import React from 'react'
+import { doc, onSnapshot } from "firebase/firestore";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
+import { db } from "../firebase";
 
-const chats = () => {
+const Chats = () => {
+    const [chats, setChats] = useState([]);
+
+    const { currentUser } = useContext(AuthContext);
+    const { dispatch } = useContext(ChatContext);
+
+    useEffect(() => {
+        const getChats = () => {
+            const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+                setChats(doc.data());
+            });
+
+            return () => {
+                unsub();
+            };
+        };
+
+        currentUser.uid && getChats();
+
+    }, [currentUser.uid]);
+
+    const handleSelect = (u) => {
+        dispatch({ type: "CHANGE_USER", payload: u });
+    };
+
+    // console.log(Object.entries(chats))
     return (
         <div className='chats'>
-            <div className="userChat" >
-                <img src="https://uploadfile.bizhizu.cn/2017/0701/20170701031430776.jpg" alt="" />
-                <div className="userChatInfo">
-                    <span>jane</span>
-                    <p>Hello</p>
+            {Object.entries(chats)?.map((chat) => (
+                <div className="userChat" key={chat[0]} onClick={() => handleSelect(chat[1].userInfo)} >
+                    <img src={chat[1].userInfo.photoURL} alt="" />
+                    <div className="userChatInfo">
+                        <span>{chat[1].userInfo.displayName}</span>
+                        <p>{chat[1].userInfo.lastMessage?.text}</p>
+                    </div>
                 </div>
-            </div>
-            <div className="userChat" >
-                <img src="https://uploadfile.bizhizu.cn/2017/0701/20170701031430776.jpg" alt="" />
-                <div className="userChatInfo">
-                    <span>jane</span>
-                    <p>Hello</p>
-                </div>
-            </div>
-            <div className="userChat" >
-                <img src="https://uploadfile.bizhizu.cn/2017/0701/20170701031430776.jpg" alt="" />
-                <div className="userChatInfo">
-                    <span>jane</span>
-                    <p>Hello</p>
-                </div>
-            </div>
+            ))}
         </div>
     )
 }
 
-export default chats
+export default Chats
